af3bcf1a76200c39ab3666f3c128c6fe074d3f1b
gabL_CS <112206749+gabLCS@users.noreply.github.com>
2025-12-05 15:44:38 -0300

    Migração JWT: Remove Flask-Login, inicializa JWT e configura cookies.
    Remove LoginManager. Adiciona JWTManager e configura JWT_TOKEN_LOCATION para cookies. Desativa JWT_COOKIE_CSRF_PROTECT para permitir o uso em templates.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from datetime import datetime
+from datetime import timedelta 
 from dotenv import load_dotenv
 import os
 
+from flask_jwt_extended import JWTManager 
+
 db = SQLAlchemy()
-login_manager = LoginManager()
+jwt = JWTManager() 
 
 def create_app():
     app = Flask(__name__)
@@ -15,16 +16,37 @@ def create_app():
 
     key = os.getenv('SECRET_KEY')
     database_url = os.getenv('DATABASE_URL')
-
-    print(f"SECRET_KEY: {key}")
-    print(f"DATABASE_URL: {database_url}")
-
+    
     app.config['SECRET_KEY'] = key
     app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+    
+
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=30) 
+    app.config["JWT_SECRET_KEY"] = key 
+    
+
+    app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+    
+
+    app.config["JWT_HEADER_NAME"] = ""
+    app.config["JWT_QUERY_STRING_NAME"] = ""
+    app.config["JWT_JSON_KEY"] = ""
+    app.config["JWT_ERROR_MESSAGE_KEY"] = "message"
+
+    app.config["JWT_COOKIE_SECURE"] = False        
+    app.config["JWT_COOKIE_SAMESITE"] = "Lax"      
+    app.config["JWT_ACCESS_COOKIE_PATH"] = "/"
+    app.config["JWT_COOKIE_HTTPONLY"] = True       
+    app.config["JWT_COOKIE_CSRF_PROTECT"] = False  
+    
 
     db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    
+    jwt.init_app(app) 
+
+    @jwt.unauthorized_loader
+    def unauthorized_callback(callback):
+        return redirect(url_for('main.login'))
 
     from app import routes
     app.register_blueprint(routes.bp)
